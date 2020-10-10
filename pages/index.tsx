9fad8d7221df5f11070aa7369f0ae78a22e197de
JoaoPaulo-LD <joaopauloalencarrodrigues@gmail.com>
2020-10-10 13:43:23 -0300

    Theming config finished

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 // import Head from 'next/head'
 
 import { Heading } from '@chakra-ui/core'
-import { jsx } from '@emotion/core'
-import { render } from 'react-dom'
 
 export default function Home() {
   return (
